63a4508cf11829e992a9d702eaabbf0cf049dd33
Britney Lozza <blozza@me.com>
2020-08-25 22:31:19 -0400

    Added session

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ flatpak install flathub org.mozilla.Thunderbird
 flatpak install flathub com.calibre_ebook.calibre
 flatpak install flathub com.tutanota.Tutanota
 flatpak install flathub org.signal.Signal
+flatpak install flathub network.loki.Session
 
 # Add youtube-dl
 
